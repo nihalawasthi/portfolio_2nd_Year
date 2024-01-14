9640798d4a18a248471073a7d77cfcbc46129a75
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-01-14 23:29:27 +0530

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,11 @@ import Socials from '../components/Socials'
 const Header = () => {
   return <header className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
     <div className='container mx-auto'>
-      Header
+      <div>
+        <Link href={'/'}>
+          <Image src={'/logo.svg'} width={220} height={48} priority={true} />
+        </Link>
+      </div>
     </div>
     </header>;
 };
