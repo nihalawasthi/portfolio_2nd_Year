1c4b1e7f9e5e6ffbbfbad3181eee304de28e73a5
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-01-14 23:13:56 +0530

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
-
+import Image from "next/image";
+import Link from "next/link";
+import Socials from '../components/Socials'
 
 const Header = () => {
-  return <header className='absolute z-30 w-full'>Header</header>;
+  return <header className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
+    <div className='container mx-auto'>
+      Header
+    </div>
+    </header>;
 };
 
 export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -8,11 +8,9 @@ const sora = Sora({
 
 import Nav from '../components/Nav'
 import Header from '../components/Header'
-import TopLeftImg from '../components/TopLeftImg'
 
 const Layout = ({children}) => {
   return <div className={'page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative'}>
-    <TopLeftImg />
     <Nav />
     <Header />
     {children}
