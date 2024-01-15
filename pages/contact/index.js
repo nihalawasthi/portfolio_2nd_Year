de0d9745c06be097e159352a6ec7cfddf25e97d7
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-01-15 20:24:27 +0530

    Add files via upload

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,4 +1,3 @@
-import Circles from '/components/Circles';
 import { BsArrowRight } from 'react-icons/bs';
 import { motion } from 'framer-motion'; // Import motion from 'framer-motion'
 import { fadeIn } from '../../variants';
@@ -15,7 +14,7 @@ const Contact = () => {
             exit='hidden'
             className='h2 text-center mb-12'
           >
-            Let's <span className='text-accent'>connect.</span>
+            Let&apos;s <span className='text-accent'>connect.</span>
           </motion.h2>
           <motion.form
             variants={fadeIn('up', 0.4)}
@@ -23,7 +22,7 @@ const Contact = () => {
             animate='show'
             exit='hidden'
             className='flex-1 flex flex-col w-full mx-auto gap-6'
-            action="mailto:nihalawasthi498@gmail.com" method="post" enctype="text/plain"
+            action="mailto:nihalawasthi498@gmail.com" method="post" encType="text/plain"
           >
             <div className='flex gap-x-6 w-full'>
               <input type='text' placeholder='name' className='input' />
@@ -33,7 +32,7 @@ const Contact = () => {
             <textarea placeholder='message' className='textarea'></textarea>
             <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
               <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
-                Let's talk
+                Let&apos;s talk
               </span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
             </button>
