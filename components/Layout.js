40124203d5a52d7d74d4c6d80f61f3206688bd7a
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-01-30 18:08:17 +0530

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -7,14 +7,28 @@ const sora = Sora({
 });
 
 import Nav from '../components/Nav'
+import Head from 'next/head';
 import Header from '../components/Header'
+
+export const metadata = {
+  title: 'Nihal Awasthi',
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/favicon.icns?v=4']
+  },
+  description: '',
+}
 
 const Layout = ({children}) => {
   return <div className={'page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative'}>
+    <Head>
+      <title>{metadata.title}</title>
+      {metadata.icons && metadata.icons.icon && <link rel="icon" href={metadata.icons.icon[0]} />}
+      {metadata.icons && metadata.icons.apple && <link rel="apple-touch-icon" href={metadata.icons.apple[0]} />}
+    </Head>
     <Nav />
     <Header />
     {children}
   </div>;
 };
-
 export default Layout;
